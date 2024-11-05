aa8c1b44f8a308526e18426a7e3a6254fa59ab9c
Spike <l-yanlei@hotmail.com>
2024-11-05 10:38:15 +0800

    :construction_worker: print build info

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# only build when `publish` change
+# Enable command printing (debug mode)
+set -x
+
+# Only build when `publish` change
+echo "Checking for changes in the 'publish' directory..."
 git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF publish
 
 # index.xml is for https://www.v2ex.com/xna
+echo "Copying rss.xml to index.xml..."
 cp -f publish/rss.xml publish/index.xml
+
+# Disable debug mode
+set +x
