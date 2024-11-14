c60e8d417b04afe3db1fb0ed15411168bd57ce64
Spike <l-yanlei@hotmail.com>
2024-11-14 19:55:20 +0800

    :construction_worker: update pagefind source folder

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF publish
 # index.xml is for https://www.v2ex.com/xna
 cp -f publish/rss.xml publish/index.xml
 
-npx -y pagefind --site public
+npx -y pagefind --site publish
 
 # Disable debug mode
 set +x
