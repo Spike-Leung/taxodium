f49a8660b9e501c08e5583666085481fb894b439
Spike <l-yanlei@hotmail.com>
2024-11-15 09:14:05 +0800

    :construction_worker: make pagefind index multilingual

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF publish
 # index.xml is for https://www.v2ex.com/xna
 cp -f publish/rss.xml publish/index.xml
 
-npx -y pagefind --site publish
+# see: https://pagefind.app/docs/multilingual/#opting-out-of-multilingual-search
+npx -y pagefind --site publish --force-language zh-CN
 
 # Disable debug mode
 set +x
