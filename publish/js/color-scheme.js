1b2870519f029911883508d517136333c470af16
Spike <l-yanlei@hotmail.com>
2024-12-20 00:28:50 +0800

    :art: format color-theme.js

--- publish/js/color-scheme.js
+++ publish/js/color-scheme.js
@@ -4,6 +4,11 @@ function getSavedColorScheme() {
   return localStorage.getItem("color-scheme") || "auto";
 }
 
+/**
+ * 主题下拉框切换事件，切换主题
+ * @param { 'auto' | 'dark' | 'light' } mode - 当前主题模式
+ * @param {boolean} isInit - 是否是页面初始化
+ */
 function switchMode(mode, isInit = false) {
   try {
     const scheme = mode === "auto" ? "light dark" : mode;
@@ -27,28 +32,63 @@ function switchMode(mode, isInit = false) {
   }
 }
 
+/**
+ * 设置主题模式下拉框的值
+ * @param { 'auto' | 'dark' | 'light' } mode - 当前主题模式
+ */
+function setModeSelectValue(mode) {
+  const modeSelect = document.querySelector("#lightdark");
+  modeSelect && (modeSelect.value = mode);
+}
+
+/**
+ * 切换 Gitcus 的主题
+ * @param { 'auto' | 'dark' | 'light' } mode - 当前主题模式
+ */
 function switchGiscusTheme(mode) {
-  const iframe = document.querySelector("iframe.giscus-frame");
   const theme = {
     auto: "preferred_color_scheme",
     light: "light_high_contrast",
     dark: "dark_high_contrast",
   };
 
-  // {@link https://github.com/giscus/giscus/issues/336}
-  function sendMessage(message) {
-    const iframe = document.querySelector("iframe.giscus-frame");
-    if (!iframe) return;
-    iframe.contentWindow.postMessage({ giscus: message }, "https://giscus.app");
-  }
+  try {
+    // {@link https://github.com/giscus/giscus/issues/336}
+    function sendMessage(message) {
+      const iframe = document.querySelector("iframe.giscus-frame");
+      if (!iframe) return;
+      iframe.contentWindow.postMessage({ giscus: message }, "https://giscus.app");
+    }
 
-  sendMessage({
-    setConfig: {
-      theme: theme[mode],
-    },
-  });
+    sendMessage({
+      setConfig: {
+        theme: theme[mode],
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
+/**
+ * 初始化 Giscus 主题
+ * @param { 'auto' | 'dark' | 'light' } mode - 当前主题模式
+ */
+function initGiscusTheme(mode) {
+  const interval = setInterval(() => {
+    // 因为 giscus 是加载 script 后动态渲染，DOMContentLoaded 时可能还没能拿到，增加一定的定时等待
+    const iframe = document.querySelector("iframe.giscus-frame");
+    if (!iframe) return;
+    iframe.addEventListener("load", () => {
+      switchGiscusTheme(mode);
+    });
+    clearInterval(interval);
+  }, 1000);
+}
+
+/**
+ * 初始化主题
+ */
 function initColorScheme() {
   try {
     const savedMode = getSavedColorScheme();
@@ -59,23 +99,11 @@ function initColorScheme() {
   }
 }
 
-function setModeSelectValue(mode) {
-  const modeSelect = document.querySelector("#lightdark");
-  modeSelect && (modeSelect.value = mode);
-}
 
 initColorScheme();
 
 document.addEventListener("DOMContentLoaded", () => {
   const savedMode = getSavedColorScheme();
-  const interval = setInterval(() => {
-    // 因为 giscus 是加载 script 后动态渲染，DOMContentLoaded 时可能还没能拿到，增加一定的定时等待
-    const iframe = document.querySelector("iframe.giscus-frame");
-    if (!iframe) return;
-    iframe.addEventListener("load", () => {
-      switchGiscusTheme(savedMode);
-    });
-    clearInterval(interval);
-  }, 1000);
+  initGiscusTheme(savedMode)
   setModeSelectValue(savedMode);
 });
