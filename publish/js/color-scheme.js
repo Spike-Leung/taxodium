29b3a6c1ddd036a612bbf318069807e7c9a17f9f
Spike <l-yanlei@hotmail.com>
2024-12-19 15:49:34 +0800

    :bug: wait giscus ready

--- publish/js/color-scheme.js
+++ publish/js/color-scheme.js
@@ -19,25 +19,34 @@ function switchMode(mode, isInit = false) {
       switchGiscusTheme(mode);
     }
 
-    isInit ? setColorScheme() : document.startViewTransition(() => setColorScheme());
+    isInit
+      ? setColorScheme()
+      : document.startViewTransition(() => setColorScheme());
   } catch (err) {
     console.error(err);
   }
 }
 
 function switchGiscusTheme(mode) {
-  const iframe = document.querySelector('.giscus-frame');
+  const iframe = document.querySelector("iframe.giscus-frame");
   const theme = {
-    auto: 'preferred_color_scheme',
-    light: 'light_high_contrast',
-    dark: 'dark_high_contrast'
+    auto: "preferred_color_scheme",
+    light: "light_high_contrast",
+    dark: "dark_high_contrast",
   };
 
-  if (iframe) {
-    const url = new URL(iframe.src);
-    url.searchParams.set('theme', theme[mode]);
-    iframe.src = url.toString();
+  // {@link https://github.com/giscus/giscus/issues/336}
+  function sendMessage(message) {
+    const iframe = document.querySelector("iframe.giscus-frame");
+    if (!iframe) return;
+    iframe.contentWindow.postMessage({ giscus: message }, "https://giscus.app");
   }
+
+  sendMessage({
+    setConfig: {
+      theme: theme[mode],
+    },
+  });
 }
 
 function initColorScheme() {
@@ -51,7 +60,7 @@ function initColorScheme() {
 }
 
 function setModeSelectValue(mode) {
-  const modeSelect = document.querySelector('#lightdark');
+  const modeSelect = document.querySelector("#lightdark");
   modeSelect && (modeSelect.value = mode);
 }
 
@@ -59,6 +68,12 @@ initColorScheme();
 
 document.addEventListener("DOMContentLoaded", () => {
   const savedMode = getSavedColorScheme();
-  switchGiscusTheme(savedMode);
+  setTimeout(() => {
+    // 因为 giscus 是加载 script 后动态渲染，DOMContentLoaded 时可能还没能拿到，增加一定的定时等待
+    const iframe = document.querySelector("iframe.giscus-frame");
+    iframe.addEventListener("load", () => {
+      switchGiscusTheme(savedMode);
+    });
+  }, 1000);
   setModeSelectValue(savedMode);
 });
