37674adf7cfef9f467c4cb9a421a3d7a7f1ddcee
Spike <l-yanlei@hotmail.com>
2025-04-07 20:58:20 +0800

    :wrench: build atom feed when trigger deploy

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ set -x
 # Only build when `publish` change
 git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF publish
 
+npm install
+
+node feed.js
+
 # index.xml is for https://www.v2ex.com/xna
 cp -f publish/rss.xml publish/index.xml
 
