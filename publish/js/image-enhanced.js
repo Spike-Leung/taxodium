881e1b0b4b8e74a9ad53eb2cb84ad7f2a87c31a8
Spike <l-yanlei@hotmail.com>
2025-07-01 17:18:51 +0800

    :sparkles: add image-enhanced script to blog

---
+++ publish/js/image-enhanced.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.matchMedia('(min-width: 768px)').matches) {
+    const images = document.querySelectorAll('#content img');
+
+    images.forEach(img => {
+      if (img.closest('a')) {
+        return;
+      }
+
+      img.style.cursor = 'pointer';
+      img.addEventListener('click', () => {
+        window.open(img.src, '_blank', 'noopener,noreferrer');
+      });
+    });
+  }
+});
