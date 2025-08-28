6e6b4b9366ab8a18e0dee0d274b034d6b74f7a78
Spike <l-yanlei@hotmail.com>
2025-08-28 13:39:34 +0800

    :wrench: add denote local config

---
+++ posts/.dir-locals.el
@@ -0,0 +1,18 @@
+;;; Directory Local Variables.  For more information evaluate:
+;;;
+;;;     (info "(emacs) Directory Variables")
+;;; see: https://protesilaos.com/emacs/denote#h:fed09992-7c43-4237-b48f-f654bc29d1d8
+((nil . ((denote-directory . "~/git/taxodium/posts")
+         (denote-org-front-matter .
+                                  "#+title:      %s
+#+date:       %s
+#+filetags:   %s
+#+identifier: %s
+#+bind:       denote-directory \"~/git/taxodium/posts\"
+#+description: Zine 第 25 期
+#+language: zh-CN
+#+author: Spike Leung
+#+email: l-yanlei@hotmail.com
+#+options: ^:{}
+#+startup: content indent
+\n"))))
