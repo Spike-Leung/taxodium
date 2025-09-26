acb3189c585cac2c17c3ee5654090110d0fc0e74
Spike <l-yanlei@hotmail.com>
2025-09-26 10:22:18 +0800

    :recycle: update webmention.js

--- publish/js/webmention.js
+++ publish/js/webmention.js
@@ -2,7 +2,7 @@ async function loadWebmentionCount() {
   try {
     const target = location.href
     const response = await fetch(`https://webmention.io/api/count?target=${target}/page/100`)
-    const { count } = response.json()
+    const { count = 0 } = await response.json()
     document.querySelector('.webmention-count').innerText = `(${count})`
   } catch (err) {
     console.error(err)
@@ -13,7 +13,7 @@ async function loadWebmentionContent() {
   try {
     const target = location.href
     const response = await fetch(`https://webmention.io/api/mentions.jf2?target=${target}/page/100`)
-    const responseJson = response.json()
+    const responseJson = await response.json()
     console.log({ responseJson })
   } catch (err) {
     console.error(err)
