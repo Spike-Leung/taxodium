a563abe10776ee73bf3e38f6e7d96770ae7fed8c
Spike <l-yanlei@hotmail.com>
2025-09-27 20:33:14 +0800

    :recycle: beautify webmention

--- publish/js/webmention.js
+++ publish/js/webmention.js
@@ -36,8 +36,6 @@ function getTargetUrl() {
 }
 
 function renderWebmentions(feed, container) {
-  const CONTENT_MAX_LENGTH = 500;
-
   if (!feed || !Array.isArray(feed.children) || !container) return;
 
   const frag = document.createDocumentFragment();
@@ -48,50 +46,54 @@ function renderWebmentions(feed, container) {
 
     const li = document.createElement("li");
 
-    const pLink = document.createElement("p");
-    const a = document.createElement("a");
-    const author =
-      entry.author && entry.author.name ? entry.author.name : "Unknown";
-    a.href = entry.url || entry["wm-source"] || "#";
-    a.textContent = `${entry.name || a.href} | ${author}`;
-    pLink.appendChild(a);
+    // author info
+    const divAuthor = document.createElement("div");
+    divAuthor.className = "webmention-author-container"
 
-    const pDate = document.createElement("p");
-    pDate.className = "webmention-date";
-    const wmReceived = new Date(entry["wm-received"]);
-    const parts = new Intl.DateTimeFormat("zh-CN", {
+    const { name, url, photo } = entry.author || {}
+
+    const imgAvatar = document.createElement("img");
+    imgAvatar.className = "webmention-avatar"
+    imgAvatar.src = photo || "/images/common/no-profile-photo.png"
+
+    const wmSourceUrl = new URL(entry["wm-source"])
+
+    const aAuthor = document.createElement("a");
+    aAuthor.href = url || wmSourceUrl.origin
+    aAuthor.className = "webmention-author"
+    aAuthor.textContent = name || "Unknown"
+
+    const aHomeUrl = document.createElement("a");
+    aHomeUrl.href = url || wmSourceUrl.origin
+    aHomeUrl.className = "webmention-home-url"
+    aHomeUrl.textContent = wmSourceUrl.host
+
+
+    Array.from([imgAvatar, aAuthor, aHomeUrl]).forEach((child) => divAuthor.appendChild(child))
+
+    // content
+    const divContent = document.createElement("div");
+    divContent.className = "webmention-content"
+    divContent.innerHTML = entry.content || ""
+
+    // meta info
+    const divMeta = document.createElement("div");
+    divMeta.className = "webmention-meta"
+    const aSource = document.createElement("a");
+    const pReceived = document.createElement("p");
+
+    aSource.href = wmSourceUrl.href
+    aSource.textContent = "Source"
+
+    pReceived.textContent = new Intl.DateTimeFormat("zh-CN", {
+      dateStyle: "full",
+      timeStyle: "long",
       timeZone: "Asia/Shanghai",
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-    }).formatToParts(wmReceived);
-    const y = parts.find((p) => p.type === "year").value;
-    const m = parts.find((p) => p.type === "month").value;
-    const day = parts.find((p) => p.type === "day").value;
-    pDate.textContent = `${y}-${m}-${day}`;
+    }).format(new Date(entry["wm-received"]))
+    pReceived.className = "webmention-date"
 
-    const blockquote = document.createElement("blockquote");
-    const pContent = document.createElement("p");
-    let contentText = "";
-    if (entry.content) {
-      if (typeof entry.content === "string") {
-        contentText = entry.content;
-      } else {
-        contentText = entry.content.text || "";
-      }
-    }
-
-    if (contentText.length > CONTENT_MAX_LENGTH) {
-      contentText = contentText.slice(0, CONTENT_MAX_LENGTH);
-      contentText += "[...]";
-    }
-
-    pContent.textContent = contentText;
-    blockquote.appendChild(pContent);
-
-    li.appendChild(pLink);
-    li.appendChild(pDate);
-    li.appendChild(blockquote);
+    Array.from([aSource, pReceived]).forEach((child) => divMeta.appendChild(child))
+    Array.from([divAuthor, divContent, divMeta]).forEach((child) => li.appendChild(child))
     frag.appendChild(li);
   }
 
