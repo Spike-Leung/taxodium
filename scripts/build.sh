da5ef3d2aa5f5f2ac3a4ac0a393a36057f8f52b0
Spike <l-yanlei@hotmail.com>
2026-04-13 16:53:15 +0800

    :recycle: remove pagefind

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,4 @@ node ./scripts/feed.js
 # copy rss.xml to index.xml, use for https://www.v2ex.com/xna
 cp -f publish/rss.xml publish/index.xml
 
-# pagefind index, for search
-npx -y pagefind --site publish --force-language zh-CN
-
 set +x
