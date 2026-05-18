f90e803bc21b6b84dff7357160453380942696b2
Spike <l-yanlei@hotmail.com>
2026-05-18 10:49:16 +0800

    :recycle: update feed footer

--- scripts/feed.js
+++ scripts/feed.js
@@ -30,10 +30,10 @@ const CONFIG = {
   outputFile: path.join(__dirname, "..", "publish/rss.xml"),
   postamble: `
 <hr>
-<p>
-感谢你的阅读！
-如果有什么想法，也欢迎 <a href="mailto:l-yanlei@hotmail.com">邮件跟我交流</a> (推荐使用 <a href="https://useplaintext.email/">纯文本邮件</a> 回复)。下篇文章再见啦 :)
-</p>
+<p>感谢你的阅读！</p>
+<p>欢迎 <a href="mailto:l-yanlei@hotmail.com">邮件</a> 跟我分享你的想法 :)</p>
+<p>你也可以<a href="https://taxodium.ink/subscribe.html">訂閱</a>我的博客，保持更新 :P</p>
+<p>祝好，素未某面的读者。</p>
 `
 };
 
