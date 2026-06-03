ce655e60951a81d3ddc726d6e22f58f780e76975
Spike <l-yanlei@hotmail.com>
2026-06-03 13:43:25 +0800

    :recycle: only query webmention data related to current page

--- publish/js/webmention.js
+++ publish/js/webmention.js
@@ -1,136 +1,130 @@
-async function loadWebmentionCount() {
-  try {
-    const target = getTargetUrl();
-    const response = await fetch(
-      `https://webmention.io/api/count?target=${target}`,
-    );
-    const { count = 0 } = await response.json();
-    document.querySelector(".webmention__count").innerText = `(${count})`;
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-async function loadWebmentionContent() {
-  try {
-    const target = getTargetUrl();
-    const response = await fetch(
-      `https://webmention.io/api/mentions.jf2?domain=taxodium.ink&token=qcwPCX61g9khbvZWp3U0qg`,
-    );
-    const feed = await response.json();
-    const feedList = feed?.children?.filter((c) => c["wm-target"].indexOf(target) !== -1)
-    document.querySelector(".webmention__count").innerText = `(${feedList.length})`;
-    const container = document.querySelector(".webmention__list");
-    renderWebmentions(feedList, container);
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-function getTargetUrl() {
-  const { href, hostname, pathname } = location;
-
-  if (hostname !== "taxodium.ink") {
-    return `https://taxodium.ink${pathname}`;
+(function () {
+  const TargetFragments = {
+    'https://taxodium.ink/43.html': ['#82F4E24E-345E-48C9-9A3B-567A81BC40A0']
   }
 
-  return href;
-}
+  async function loadWebmentionContent() {
+    try {
+      const target = getTargetUrl();
+      const targetsWithFragments = TargetFragments[target] ? TargetFragments[target].map((hash) => `${target}${encodeURIComponent(hash)}`) : []
+      const allTarget = [target, ...targetsWithFragments];
+      const searchParams = allTarget.map((t) => `target[]=${t}`).join("&")
+      const response = await fetch(`https://webmention.io/api/mentions.jf2?${searchParams}`,);
+      const feed = await response.json();
+      const feedList = feed?.children?.filter((c) => c["wm-target"].indexOf(target) !== -1)
+      document.querySelector(".webmention__count").innerText = `(${feedList.length})`;
+      const container = document.querySelector(".webmention__list");
+      renderWebmentions(feedList, container);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
-function renderWebmentions(feedList = [], container) {
-  if (feedList.length === 0 || !container) return;
+  function getTargetUrl() {
+    const { href, hostname, pathname } = location;
 
-  const frag = document.createDocumentFragment();
+    if (hostname !== "taxodium.ink") {
+      return `https://taxodium.ink${pathname}`;
+    }
 
-  for (const entry of feedList) {
-    if (entry.type !== "entry") continue;
-    if (entry["wm-private"] === true) continue;
+    return href;
+  }
 
-    const li = document.createElement("li");
-    li.className = "webmention-card"
+  function renderWebmentions(feedList = [], container) {
+    if (feedList.length === 0 || !container) return;
 
-    // author info
-    const divProfile = document.createElement("div");
-    divProfile.className = "webmention__profile"
+    const frag = document.createDocumentFragment();
 
-    const { name, url, photo } = entry.author || {}
+    for (const entry of feedList) {
+      if (entry.type !== "entry") continue;
+      if (entry["wm-private"] === true) continue;
 
-    const imgAvatar = document.createElement("img");
-    imgAvatar.className = "webmention__avatar"
-    imgAvatar.src = photo || "/images/common/no-profile-photo.png"
+      const li = document.createElement("li");
+      li.className = "webmention-card"
 
-    const wmSourceUrl = new URL(entry["wm-source"])
+      // author info
+      const divProfile = document.createElement("div");
+      divProfile.className = "webmention__profile"
 
-    const aAuthor = document.createElement("a");
-    aAuthor.href = url || wmSourceUrl.origin
-    aAuthor.className = "webmention__author"
-    aAuthor.textContent = name || wmSourceUrl.host || "Unknown"
+      const { name, url, photo } = entry.author || {}
 
-    let textFragments = ''
-    let quotes
-    if (entry.content && entry.content.html) {
-      const match = entry.content.html.match(/<a.*href=.*taxodium.ink[^>]*>([^<]*)<\/a>/)
-      if (match && match[1]) {
-        const matchText = match[1].trim()
-        const originalText = entry.content.text
-        const index = originalText.indexOf(matchText)
+      const imgAvatar = document.createElement("img");
+      imgAvatar.className = "webmention__avatar"
+      imgAvatar.src = photo || "/images/common/no-profile-photo.png"
 
-        if (index !== -1) {
-          const offset = 100
-          const contextBefore = originalText.slice(Math.max(0, index - offset), index)
-          const contextAfter = originalText.slice(index + matchText.length, Math.min(index + matchText.length + offset, originalText.length))
-          quotes = `[...]${contextBefore}<mark>${matchText}</mark>${contextAfter}[...]`
-          const encodedMatch = encodeURIComponent(matchText)
+      const wmSourceUrl = new URL(entry["wm-source"])
 
-          textFragments = `#:~:text=${encodedMatch}`
+      const aAuthor = document.createElement("a");
+      aAuthor.href = url || wmSourceUrl.origin
+      aAuthor.className = "webmention__author"
+      aAuthor.textContent = name || wmSourceUrl.host || "Unknown"
+
+      let textFragments = ''
+      let quotes
+      if (entry.content && entry.content.html) {
+        const match = entry.content.html.match(/<a.*href=.*taxodium.ink[^>]*>([^<]*)<\/a>/)
+        if (match && match[1]) {
+          const matchText = match[1].trim()
+          const originalText = entry.content.text
+          const index = originalText.indexOf(matchText)
+
+          if (index !== -1) {
+            const offset = 100
+            const contextBefore = originalText.slice(Math.max(0, index - offset), index)
+            const contextAfter = originalText.slice(index + matchText.length, Math.min(index + matchText.length + offset, originalText.length))
+            quotes = `[...]${contextBefore}<mark>${matchText}</mark>${contextAfter}[...]`
+            const encodedMatch = encodeURIComponent(matchText)
+
+            textFragments = `#:~:text=${encodedMatch}`
+          }
         }
       }
+
+      // fallback to full content
+      if (!quotes) {
+        quotes = entry?.content?.html || entry?.content?.text
+      }
+
+      const aSource = document.createElement("a");
+      aSource.href = wmSourceUrl.href + textFragments
+      aSource.textContent = entry.name || wmSourceUrl.href
+      aSource.rel = "noopener"
+
+      const pSource = document.createElement("p")
+      pSource.className ="webmention__source"
+      pSource.innerText = "原文："
+      pSource.appendChild(aSource)
+
+      Array.from([imgAvatar, aAuthor, pSource]).forEach((child) => divProfile.appendChild(child))
+
+      // content
+      const divContent = document.createElement("blockquote");
+      divContent.className = "webmention__content"
+      divContent.innerHTML = DOMPurify.sanitize(quotes) || ""
+
+      const pReceivedDate = document.createElement("p");
+      pReceivedDate.textContent = new Intl.DateTimeFormat("zh-CN", {
+        dateStyle: "full",
+        timeStyle: "long",
+        timeZone: "Asia/Shanghai",
+      }).format(new Date(entry["wm-received"]))
+      pReceivedDate.className = "webmention__date"
+
+      Array.from([divProfile, divContent, pReceivedDate]).forEach((child) => li.appendChild(child))
+      frag.appendChild(li);
     }
 
-    // fallback to full content
-    if (!quotes) {
-      quotes = entry?.content?.html || entry?.content?.text
-    }
-
-    const aSource = document.createElement("a");
-    aSource.href = wmSourceUrl.href + textFragments
-    aSource.textContent = entry.name || wmSourceUrl.href
-    aSource.rel = "noopener"
-
-    const pSource = document.createElement("p")
-    pSource.className ="webmention__source"
-    pSource.innerText = "原文："
-    pSource.appendChild(aSource)
-
-    Array.from([imgAvatar, aAuthor, pSource]).forEach((child) => divProfile.appendChild(child))
-
-    // content
-    const divContent = document.createElement("blockquote");
-    divContent.className = "webmention__content"
-    divContent.innerHTML = DOMPurify.sanitize(quotes) || ""
-
-    const pReceivedDate = document.createElement("p");
-    pReceivedDate.textContent = new Intl.DateTimeFormat("zh-CN", {
-      dateStyle: "full",
-      timeStyle: "long",
-      timeZone: "Asia/Shanghai",
-    }).format(new Date(entry["wm-received"]))
-    pReceivedDate.className = "webmention__date"
-
-    Array.from([divProfile, divContent, pReceivedDate]).forEach((child) => li.appendChild(child))
-    frag.appendChild(li);
+    container.appendChild(frag);
   }
 
-  container.appendChild(frag);
-}
+  function initFormTargetUrl() {
+    const target = getTargetUrl();
+    document.querySelector(".webmention form input[name='target']").value = target;
+  }
 
-function initFormTargetUrl() {
-  const target = getTargetUrl();
-  document.querySelector(".webmention form input[name='target']").value = target;
-}
+  document.addEventListener("DOMContentLoaded", function () {
+    loadWebmentionContent();
+    initFormTargetUrl();
+  });
 
-document.addEventListener("DOMContentLoaded", function () {
-  // loadWebmentionCount();
-  loadWebmentionContent();
-  initFormTargetUrl();
-});
+})();
