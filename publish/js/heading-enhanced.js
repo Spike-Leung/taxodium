2d275e656453f0568726a25216533985b3ff34ab
Spike <l-yanlei@hotmail.com>
2026-06-05 20:51:53 +0800

    :children_crossing: some a11y update

--- publish/js/heading-enhanced.js
+++ publish/js/heading-enhanced.js
@@ -1,14 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
+  function positionHeading(heading) {
+    heading.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+
+    history.pushState(null, null, '#' + heading.id);
+  }
+
   document.querySelectorAll('h2[id], h3[id], h4[id], h5[id], h6[id]').forEach(heading => {
+    heading.tabIndex = 0;
+
     heading.addEventListener('click', (e) => {
       if (e.target.nodeName === "A") return
 
-      heading.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      });
+      positionHeading(heading)
+    });
 
-      history.pushState(null, null, '#' + heading.id);
+    heading.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter' || e.key === ' ') {
+        e.preventDefault();
+        positionHeading(heading)
+      }
     });
   });
 })
