0a2f080af96b673bbd4b5a13b32c83d0e6e780ca
Spike <l-yanlei@hotmail.com>
2026-07-10 07:39:54 +0800

    build: update shuffle data when build

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,9 @@ pnpm install
 # build feed
 node ./scripts/feed.js
 
+# update shuffle data
+node ./scripts/generateShuffleData.js
+
 # copy rss.xml to index.xml, use for https://www.v2ex.com/xna
 cp -f publish/rss.xml publish/index.xml
 
