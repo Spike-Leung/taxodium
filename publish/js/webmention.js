526b6331aefb41464c5dc149e4d0b8915cb30314
Spike <l-yanlei@hotmail.com>
2026-09-14 23:48:47 +0800

    webmention: seperate function to small function

--- publish/js/webmention.js
+++ publish/js/webmention.js
@@ -4,24 +4,19 @@
   }
   const TargetQueries = ['ref=powrss.com']
 
-  async function loadWebmentionContent() {
-    try {
-      const target = getTargetUrl();
-      const targetUrl = new URL(target)
-      const baseTarget = `${targetUrl.origin}${targetUrl.pathname}`
-      const targetsWithFragments = TargetFragments[target] ? TargetFragments[target].map((hash) => `${target}${hash}`) : []
-      const targetsWithQueries = TargetQueries.map((q) => `${target}?${q}`)
-      const allTarget = [baseTarget, target, ...targetsWithFragments, ...targetsWithQueries];
-      const searchParams = allTarget.map((t) => `target[]=${encodeURIComponent(t)}`).join("&")
-      const response = await fetch(`https://webmention.io/api/mentions.jf2?${searchParams}`,);
-      const feed = await response.json();
-      const feedList = feed?.children?.filter((c) => c["wm-target"].indexOf(baseTarget) !== -1)
-      document.querySelector(".webmention__count").innerText = `(${feedList.length})`;
-      const container = document.querySelector(".webmention__list");
-      renderWebmentions(feedList, container);
-    } catch (err) {
-      console.error(err);
-    }
+  async function fetchWebmentionFeedList() {
+    const target = getTargetUrl();
+    const targetUrl = new URL(target)
+    const baseTarget = `${targetUrl.origin}${targetUrl.pathname}`
+    const targetsWithFragments = TargetFragments[target] ? TargetFragments[target].map((hash) => `${target}${hash}`) : []
+    const targetsWithQueries = TargetQueries.map((q) => `${target}?${q}`)
+    const allTarget = [baseTarget, target, ...targetsWithFragments, ...targetsWithQueries];
+    const searchParams = allTarget.map((t) => `target[]=${encodeURIComponent(t)}`).join("&")
+    const response = await fetch(`https://webmention.io/api/mentions.jf2?${searchParams}`);
+    const feed = await response.json()
+    const feedList = feed?.children?.filter((c) => c["wm-target"].indexOf(baseTarget) !== -1)
+
+    return feedList
   }
 
   function getTargetUrl() {
@@ -34,41 +29,72 @@
     return href;
   }
 
-  function renderWebmentions(feedList = [], container) {
-    if (feedList.length === 0 || !container) return;
+  function generateProfileHTMLFragments(entry, textFragments) {
+    const divProfile = document.createElement("div");
+    divProfile.className = "webmention__profile"
 
-    const frag = document.createDocumentFragment();
+    const { name, url, photo } = entry.author || {}
 
-    for (const entry of feedList) {
-      if (entry.type !== "entry") continue;
-      if (entry["wm-private"] === true) continue;
+    const imgAvatar = document.createElement("img");
+    imgAvatar.className = "webmention__avatar"
+    imgAvatar.src = photo || "/images/common/no-profile-photo.png"
 
-      const li = document.createElement("li");
-      li.className = "webmention__item"
+    const wmSourceUrl = new URL(entry["wm-source"])
 
-      // author info
-      const divProfile = document.createElement("div");
-      divProfile.className = "webmention__profile"
+    const aAuthor = document.createElement("a");
+    aAuthor.href = url || wmSourceUrl.origin
+    aAuthor.className = "webmention__author"
+    aAuthor.textContent = name || wmSourceUrl.host || "Unknown"
 
-      const { name, url, photo } = entry.author || {}
+    const aSource = document.createElement("a");
+    aSource.href = wmSourceUrl.href + textFragments
+    aSource.textContent = entry.name || wmSourceUrl.href
+    aSource.rel = "noopener"
 
-      const imgAvatar = document.createElement("img");
-      imgAvatar.className = "webmention__avatar"
-      imgAvatar.src = photo || "/images/common/no-profile-photo.png"
+    const pSource = document.createElement("p")
+    pSource.className ="webmention__source"
+    pSource.innerText = "原文："
+    pSource.appendChild(aSource)
 
-      const wmSourceUrl = new URL(entry["wm-source"])
+    Array.from([imgAvatar, aAuthor, pSource]).forEach((child) => divProfile.appendChild(child))
 
-      const aAuthor = document.createElement("a");
-      aAuthor.href = url || wmSourceUrl.origin
-      aAuthor.className = "webmention__author"
-      aAuthor.textContent = name || wmSourceUrl.host || "Unknown"
+    return divProfile
+  }
 
-      let textFragments = ''
-      let quotes
-      if (entry.content && entry.content.html) {
-        const match = entry.content.html.match(/(.{2})<a.*href=.*taxodium.ink[^>]*>([^<]*)<\/a>(.{2})/)
-        if (match && match[1]) {
-          const [prefixText, matchText, suffixText] = match.slice(1)
+  function generateContentHTMLFragments(content) {
+    const divContent = document.createElement("blockquote");
+    divContent.className = "webmention__content"
+    divContent.innerHTML = DOMPurify.sanitize(content) || ""
+
+    return divContent
+  }
+
+  function generateDateHTMLFragments(entry) {
+    const pReceivedDate = document.createElement("p");
+
+    pReceivedDate.textContent = new Intl.DateTimeFormat("zh-CN", {
+      dateStyle: "full",
+      timeStyle: "long",
+      timeZone: "Asia/Shanghai",
+    }).format(new Date(entry["wm-received"]))
+    pReceivedDate.className = "webmention__date"
+
+    return pReceivedDate
+  }
+
+  function getQuotesAndTextFragments(entry) {
+    let textFragments = ''
+    let quotes
+
+    if (entry.content && entry.content.html) {
+      const regexp = new RegExp(`<a[^>]*taxodium.ink${location.pathname}[^>]*>([^<]*)<\/a>`, "g")
+
+      // 可能會存在多個匹配值
+      const matches = [...entry.content.html.matchAll(regexp)]
+
+      if (matches.length > 0) {
+        const fragments = matches.map((match) => {
+          const [prefixText, matchText, suffixText] = ['', match[1], '']
           const originalText = entry.content.text
           const index = originalText.indexOf(prefixText + matchText + suffixText)
 
@@ -80,47 +106,62 @@
             quotes = `[...]${contextBefore}<mark>${matchText}</mark>${contextAfter}[...]`
 
             // @see: https://developer.mozilla.org/en-US/docs/Web/URI/Reference/Fragment/Text_fragments#syntax
-            const fragments = `${encodeURIComponent(prefixText)}-,${encodeURIComponent(matchText)}`
-            textFragments = `#:~:text=${fragments}`
+            return `${encodeURIComponent(prefixText)}-,${encodeURIComponent(matchText)}`
           }
-        }
+        }).join("&")
+
+        textFragments = `#:~:text=${fragments}`
       }
+    }
 
-      // fallback to full content
-      if (!quotes) {
-        quotes = entry?.content?.html || entry?.content?.text
-      }
 
-      const aSource = document.createElement("a");
-      aSource.href = wmSourceUrl.href + textFragments
-      aSource.textContent = entry.name || wmSourceUrl.href
-      aSource.rel = "noopener"
+    // fallback to full content
+    if (!quotes) {
+      quotes = entry?.content?.html || entry?.content?.text
+    }
 
-      const pSource = document.createElement("p")
-      pSource.className ="webmention__source"
-      pSource.innerText = "原文："
-      pSource.appendChild(aSource)
+    return { quotes, textFragments }
+  }
 
-      Array.from([imgAvatar, aAuthor, pSource]).forEach((child) => divProfile.appendChild(child))
+  function generateWebmenionItemHTMlFragments(entry) {
+    const li = document.createElement("li");
+    li.className = "webmention__item"
 
-      // content
-      const divContent = document.createElement("blockquote");
-      divContent.className = "webmention__content"
-      divContent.innerHTML = DOMPurify.sanitize(quotes) || ""
+    const { quotes, textFragments } = getQuotesAndTextFragments(entry)
+    const divProfile = generateProfileHTMLFragments(entry, textFragments)
+    const pReceivedDate = generateDateHTMLFragments(entry)
+    const divContent = generateContentHTMLFragments(quotes)
 
-      const pReceivedDate = document.createElement("p");
-      pReceivedDate.textContent = new Intl.DateTimeFormat("zh-CN", {
-        dateStyle: "full",
-        timeStyle: "long",
-        timeZone: "Asia/Shanghai",
-      }).format(new Date(entry["wm-received"]))
-      pReceivedDate.className = "webmention__date"
+    Array.from([divProfile, divContent, pReceivedDate]).forEach((child) => li.appendChild(child))
 
-      Array.from([divProfile, divContent, pReceivedDate]).forEach((child) => li.appendChild(child))
-      frag.appendChild(li);
+    return li
+  }
+
+  function renderWebmentions(feedList = [], container) {
+    if (feedList.length === 0 || !container) return;
+
+    const frag = document.createDocumentFragment();
+
+    for (const entry of feedList) {
+      if (entry.type !== "entry") continue;
+      if (entry["wm-private"] === true) continue;
+
+      const webmentionItem = generateWebmenionItemHTMlFragments(entry)
+      frag.appendChild(webmentionItem);
     }
 
     container.appendChild(frag);
+  }
+
+  async function loadWebmentionContent() {
+    try {
+      const feedList = await fetchWebmentionFeedList()
+      document.querySelector(".webmention__count").innerText = `(${feedList.length})`;
+      const container = document.querySelector(".webmention__list");
+      renderWebmentions(feedList, container);
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   function initFormTargetUrl() {
@@ -132,5 +173,4 @@
     loadWebmentionContent();
     initFormTargetUrl();
   });
-
 })();
